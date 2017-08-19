375b6c54d1d8c28c1e4ab8b0209e8eb21e0ad970
Wilma456 (Jakob0815) <wilma456@gmx.de>
2017-08-19 18:08:13 +0200

    Update to Version 1.1

--- minepack.lua
+++ minepack.lua
@@ -181,7 +181,7 @@ end
 
 local function info(sName)
     if sName == nil then
-       print("No package selected")
+       printError("minepack info <package>")
        return
     end
     minepackapi.load()
@@ -212,16 +212,58 @@ local function info(sName)
     print("Description: "..info.description)
 end
 
+local function file(sName)
+    if sName == nil then
+       printError("Usage: minepack file <file>")
+       return
+    end
+    if sName:find("/") == 1 then
+        sName = sName:sub(2)
+    end
+    minepackapi.load()
+    for k,v in pairs(minepackapi.list) do
+        if k:find("/") ~= nil then
+            if type(v.target) == "string" and type(v.download.type.filename) == "string" then
+                if fs.combine(v.target,v.download.type.filename):find(sName) ~= nil then
+                    print(k)
+                end
+                --print(fs.combine(v.target,v.download.type.filename))
+            end
+        end
+    end
+end
+
+local function log()
+    if not fs.exists(minepackapi.config.minepackDirectory.."/log.txt") then
+        print("No log available")
+        return
+    end
+    local w,h = term.getSize()
+    local file = io.open( minepackapi.config.minepackDirectory.."/log.txt" )
+    local nLinesPrinted = 0
+    local sLine = file:read()
+    local nLines = 0
+    while sLine do
+        nLines = nLines + textutils.pagedPrint( sLine, (h-3) - nLines )
+        sLine = file:read()
+    end
+    file:close()
+end
+
 local function help()
 print([[
 minepack install <package>
 minepack remove <package>
 minepack update
 minepack fetch
+minepack fetch update
 minepack list
 minepack search
 minepack info <package>
-minepack help]])
+minepack file <file>
+minepack log
+minepack help
+minepack version]])
 end
 
 local tArgs = {...}
@@ -232,6 +274,9 @@ elseif tArgs[1] == "remove" then
     remove(tArgs[2])
 elseif tArgs[1] == "update" then
     update()
+elseif tArgs[1] == "fetch" and tArgs[2] == "update" then
+    fetch("https://raw.githubusercontent.com/lyqyd/cc-packman/master/repolist")
+    update()
 elseif tArgs[1] == "fetch" then
     fetch("https://raw.githubusercontent.com/lyqyd/cc-packman/master/repolist")
 elseif tArgs[1] == "list" then
@@ -240,8 +285,14 @@ elseif tArgs[1] == "search" then
     search()
 elseif tArgs[1] == "info" then
     info(tArgs[2])
+elseif tArgs[1] == "file" then
+    file(tArgs[2])
+elseif tArgs[1] == "log" then
+    log()
 elseif tArgs[1] == "help" then
     help()
+elseif tArgs[1] == "version" then
+    print("Version 1.1")
 else
     print("Unknown Command. Please run minepack help for a list of Commands")
 end
