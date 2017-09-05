327a4f57ec9e3609d1df0a1632a9cf8277b77d9b
Wilma456 (Jakob0815) <wilma456@gmx.de>
2017-09-05 15:39:00 +0200

    Update to Version 2.0

--- packshell.lua
+++ packshell.lua
@@ -1,6 +1,9 @@
 shell.setPath(shell.path()..":/usr/bin")
 help.setPath(help.path()..":/usr/help")
 
+os.loadAPI("/usr/apis/minepackapi")
+minepackapi.load()
+
 local function completeMultipleChoice( sText, tOptions, bAddSpaces )
     local tResults = {}
     for n=1,#tOptions do
@@ -16,10 +19,26 @@ local function completeMultipleChoice( sText, tOptions, bAddSpaces )
     end
     return tResults
 end
-local tMinepack = {"install","remove","update","fetch","list","search","info","file","log","help","version"}
+local tMinepack = {"install","remove","update","fetch","list","download","search","info","file","log","help","version"}
+local tSearch = {}
+for k,v in pairs(minepackapi.list) do
+    table.insert(tSearch,k)
+end
+local tList = {}
+for k,v in pairs(minepackapi.installed) do
+    table.insert(tList,k)
+end
 local function completeMinepack( shell, nIndex, sText, tPreviousText )
     if nIndex == 1 then
         return completeMultipleChoice( sText, tMinepack, true, true )
+    elseif nIndex == 2 then
+        if tPreviousText[2] == "install" or tPreviousText[2] == "info" or tPreviousText[2] == "download" then
+            return completeMultipleChoice( sText, tSearch, true, true )
+        elseif tPreviousText[2] == "remove" then
+            return completeMultipleChoice( sText, tList, true, true )
+        elseif tPreviousText[2] == "fetch" then
+            return completeMultipleChoice( sText, {"update"}, true, true )
+        end
     end
 end
 shell.setCompletionFunction( "usr/bin/minepack.lua", completeMinepack )
